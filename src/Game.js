cc2148faac4371a6696cf14cb01c39bb2e05c232
Vlad Konovalchuk <mercurus.80@gmail.com>
2018-08-23 16:19:05 +0300

    Fix Collection Knife; Add text to knifes

--- src/Game.js
+++ src/Game.js
@@ -8,7 +8,7 @@ import knife5 from '../assets/knife5.png';
 import item1 from '../assets/item1.png';
 import arrow1 from '../assets/arr.jpg';
 import arrow2 from '../assets/arrow2.png';
-
+import coin  from '../assets/apple.png';
 
 
 // global game options
@@ -71,7 +71,7 @@ window.onload = function () {
     resize();
     window.addEventListener("resize", resize, false);
 }
-
+let softCurrency = 0;
 let style = {
     title: {
         fontFamily: 'Arial',
@@ -102,6 +102,12 @@ let style = {
         fontSize: 40,
         color: '#67ff8b',
         letterSpacing: 3,
+    },
+    collectionTitle: {
+        fontFamily: 'Arial',
+        fontSize: 58,
+        color: '#39a24e',
+        letterSpacing: 3,
     }
 };
 
@@ -122,6 +128,7 @@ class Menu extends Phaser.Scene {
     // method to be executed when the scene preloads
     preload() {
         this.load.image('back', back)
+        this.load.image('coin',coin)
     }
     create() {
         console.log(back)
@@ -204,6 +211,9 @@ class Menu extends Phaser.Scene {
             console.log('change to PlayGame')
             this.scene.start('PlayGame');
         }, this);
+
+        // this.add.image(game.config.width / 2, game.config.height/2, 'coin').setScale(0.4)
+
     }
 
     update() {
@@ -229,6 +239,7 @@ class playGame extends Phaser.Scene {
     constructor() {
         super("PlayGame");
         this.score = scores.current;
+        this.softCurrency = softCurrency;
     }
 
     // method to be executed when the scene preloads
@@ -236,13 +247,18 @@ class playGame extends Phaser.Scene {
 
         // loading assets
         this.load.image("target", item1);
-        this.load.image("knife", knife);
+        this.load.image("knife", knife2);
+        this.load.spritesheet("coin", coin, {
+            frameWidth: 70,
+            frameHeight: 96
+        })
     }
 
     // method to be executed once the scene has been created
     create() {
         console.log('PlayGame')
         this.scoreText = this.add.text(10, 10, `Score: ${this.score}`, style.scores)
+        this.coinsText = this.add.text(450, 10, `Coins: ${this.softCurrency}`, style.scores)
         // at the beginning of the game, both current rotation speed and new rotation speed are set to default rotation speed
         this.currentRotationSpeed = gameOptions.rotationSpeed;
         this.newRotationSpeed = gameOptions.rotationSpeed;
@@ -272,6 +288,9 @@ class playGame extends Phaser.Scene {
             callbackScope: this,
             loop: true
         });
+        this.createCoin()
+
+
     }
 
     // method to change the rotation speed of the target
@@ -289,7 +308,37 @@ class playGame extends Phaser.Scene {
         // setting new rotation speed limits
         this.newRotationSpeed = Phaser.Math.Clamp(this.newRotationSpeed, -gameOptions.maxRotationSpeed, gameOptions.maxRotationSpeed);
     }
+    createCoin(){
+        var coinAngle = Phaser.Math.Between(0, 360);
+        var radians = Phaser.Math.DegToRad(coinAngle - 90);
 
+        this.coin = this.add.sprite(this.target.x + (this.target.width / 2) * Math.cos(radians), this.target.y + (this.target.width / 2) * Math.sin(radians), "coin");
+        this.coin.angle = coinAngle;
+        this.coin.startAngle = coinAngle;
+        this.coin.depth = 2;
+        this.coin.hit = false;
+        console.log(`X and Y position of coin ${this.coin.x}, ${this.coin.y}`);
+        
+    }
+    createTimeOut(){
+        console.log('createTimeOut');
+        let that = this;
+        console.log(this.softCurrency);
+        
+        function randInt() {
+            var rand = Math.random() * (+that.softCurrency);
+            rand = Math.floor(rand);
+            return rand;
+          }
+          
+          function request() {
+            setTimeout(()=>{
+                console.log('creating coin');
+              that.createCoin()
+            }, randInt());
+          }
+          request();
+    }
     // method to throw a knife
     throwKnife() {
 
@@ -327,20 +376,83 @@ class playGame extends Phaser.Scene {
                     for (var i = 0; i < children.length; i++) {
 
                         // is the knife too close to the i-th knife?
-                        if (Math.abs(Phaser.Math.Angle.ShortestBetween(this.target.angle, children[i].impactAngle)) < gameOptions.minAngle) {
-
+                        if (Math.abs(Phaser.Math.Angle.ShortestBetween(this.target.angle, children[i].impactAngle)) < gameOptions.minAngle) {                            
                             // this is not a legal hit
                             legalHit = false;
                             this.score = scores.current
                             this.scoreText.setText(`Score: ${this.score}`)
+                        
                             // no need to continue with the loop
                             break;
                         }
                     }
-
-                    // is this a legal hit?
+                    // is this a legal hit
                     if (legalHit) {
-                        console.log('Win')
+                // ----------------------   ---------------------------------------------------------------
+       
+                            console.log(Math.floor(Math.abs(Phaser.Math.Angle.ShortestBetween(this.target.angle, 190 - this.coin.startAngle))) < Math.abs(gameOptions.minAngle));
+                            
+                                if(Math.abs(Phaser.Math.Angle.ShortestBetween(this.target.angle, 180 - this.coin.startAngle)) < gameOptions.minAngle && !this.coin.hit){
+
+                                    console.log('In if block  - true');
+                                    
+                                // coin has been hit
+                                this.coin.hit = !this.coin.hit;
+                                this.softCurrency+=25;
+                                // console.log(this.softCurrency);
+                                this.coinsText.setText(`Coins: ${this.softCurrency}`)
+                                // change coin frame to show one slice
+                                this.coin.setFrame(1);
+     
+                                // add the other coin slice in the same coin posiiton
+                                var slice = this.add.sprite(this.coin.x, this.coin.y, "coin", 1);
+     
+                                // same angle too.
+                                slice.angle = this.coin.angle;
+     
+                                // and same origin
+                                slice.setOrigin(0.5, 1);
+                              
+                                // tween to make coin slices fall down
+                                this.tweens.add({
+     
+                                    // adding the knife to tween targets
+                                    targets: [this.coin, slice],
+     
+                                    // y destination
+                                    y: game.config.height + this.coin.height,
+     
+                                    // x destination
+                                    x: {
+     
+                                        // running a function to get different x ends for each slice according to frame number
+                                        getEnd: function(target, key, value){
+                                            return Phaser.Math.Between(0, game.config.width / 2) + (game.config.width / 2 * (target.frame.name - 1));
+                                        }
+                                    },
+     
+                                    // rotation destination, in radians
+                                    angle: 45,
+     
+                                    // tween duration
+                                    duration: gameOptions.throwSpeed * 6,
+     
+                                    // callback scope
+                                    callbackScope: this,
+     
+                                    // function to be executed once the tween has been completed
+                                    onComplete: function(tween){
+                                        // this.createCoin()
+                                        this.coin.hit = !this.coin.hit;
+                                        this.createTimeOut()
+                                        // restart the game
+                                        // this.scene.start("PlayGame")
+                                    }
+                                });
+                            }
+                        
+                            
+// ------------------------------------------------------------------------------
                         // player can now throw again
                         this.canThrow = true;
                         this.score++;
@@ -360,8 +472,6 @@ class playGame extends Phaser.Scene {
 
                     // in case this is not a legal hit
                     else {
-                        console.log('Loose')
-
                         // tween to throw the knife
                         this.tweens.add({
 
@@ -385,11 +495,7 @@ class playGame extends Phaser.Scene {
                                 // restart the game
                                 this.scene.start("PlayGame")
                             },
-
-
                         });
-                        // this.score = scores.current
-                        // this.scoreText.setText(`Score: ${this.score}`)
                     }
                 }
             });
@@ -421,6 +527,20 @@ class playGame extends Phaser.Scene {
 
         // adjusting current rotation speed using linear interpolation
         this.currentRotationSpeed = Phaser.Math.Linear(this.currentRotationSpeed, this.newRotationSpeed, delta / 1000);
+
+           // if the coin has not been hit...
+           if(!this.coin.hit){
+ 
+            // adjusting coin rotation
+            this.coin.angle += this.currentRotationSpeed;
+ 
+            // turning coin angle in radians
+            var radians = Phaser.Math.DegToRad(this.coin.angle - 90);
+ 
+            // adjusting coin position
+            this.coin.x = this.target.x + (this.target.width / 2) * Math.cos(radians);
+            this.coin.y = this.target.y + (this.target.width / 2) * Math.sin(radians);
+        }
     }
 }
 
@@ -504,6 +624,16 @@ class Collection extends Phaser.Scene {
     }
     create() {
         var self = this;
+        this.add.image(game.config.width + 190, game.config.height, 'back').setOrigin(1).setScale(1)
+        this.collectionTitle = this.add.text(game.config.width / 2, 300, 'Choose Weapon', style.collectionTitle)
+            .setOrigin(0.5)
+        this.backBtn = this.add.text(game.config.width / 2, 50, 'Back to menu', style.title)
+            .setOrigin(0.5)
+            .setInteractive()
+        this.backBtn.on('pointerdown', function (event) {
+            console.log('change to menu')
+            this.scene.start('menu');
+        }, this);
         this.rightArrow = this.add.sprite(game.config.width / 2 + 280, game.config.height / 2, 'rightArrow');
         this.rightArrow.setScale(0.3)
         this.rightArrow.customParams = {
@@ -525,34 +655,39 @@ class Collection extends Phaser.Scene {
             {
                 key: 'knife2',
                 text: 'Winter',
-                price: 100,
+                price: 179,
             },
             {
                 key: 'knife3',
                 text: 'Scalpel',
-                price: 100,
+                price: 200,
             },
             {
                 key: 'knife4',
                 text: 'Hunting Knife',
-                price: 100,
+                price: 321,
             },
             {
                 key: 'knife5',
                 text: 'Bayonets',
-                price: 100,
+                price: 142,
             }
         ]
         this.knifes = this.add.group();
-        // knifeData.forEach(element => {
-        //     this.knifes.create(game.config.width / 2, game.config.height / 2, element.key)
-        // });
+        // Creating group of knifes
+        this.knifesNames = this.add.group();
+        // Creating basic structure for creating knife "slider"
         let i = 0;
         let indexedKnife;
         for (let element of knifeData) {
-            indexedKnife = this.knifes.create(-1000, game.config.height / 2, element.key)
-            indexedKnife.setInteractive();
+            indexedKnife = this.knifes.create(-1000, game.config.height / 2, element.key);
+            // textKnife = this.add.text(200, game.config.height / 2 + 100, knifeData[0].text,style.title)
 
+            indexedKnife.customParams = {
+                text: element.text,
+                price: element.price,
+                count: i
+            }
             // This will keep track of the current knife in the knifes group.
             this.knifes.next = (i) => {
                 return this.knifes.children.entries[i];
@@ -704,27 +839,47 @@ class Collection extends Phaser.Scene {
 
 
         }
+
         function onCompleteHandle(tween, targets, myImage) {
             self.isMoving = false;
         }
 
     }
     showText(knife) {
-        if (!this.knifeText) {
+        if (!this.knifeText && !this.knifePrice) {
+            this.knifePrice = this.add.text(
+                game.config.width / 2,
+                game.config.height / 2 + 300,
+                // One thing to keep in mind is that align only works with multi-line text, so we won't use it.
+                knife.customParams.price, {
+                    fontFamily: 'Arial',
+                    fontSize: '60px',
+                    color: '#af0d00'
+                }
+            );
+            console.log('price end');
+            this.knifePrice.setOrigin(0.5);
+            console.log(knife.customParams.price)
+
             this.knifeText = this.add.text(
-                this.currentKnife.x * 0.95,
-                game.config.height / 2,
+                game.config.width / 2,
+                game.config.height / 2 + 200,
                 // One thing to keep in mind is that align only works with multi-line text, so we won't use it.
                 knife.customParams.text, {
                     fontFamily: 'Arial',
-                    fontSize: '2em',
-                    color: '#67d0ce'
+                    fontSize: '60px',
+                    color: '#af0d00'
                 }
             );
+            this.knifeText.setOrigin(0.5);
+            console.log('price start');
+
+
         } else {
+            this.knifePrice.setText(knife.customParams.price)
             this.knifeText.setText(knife.customParams.text);
-            console.log(knife)
             this.knifeText.visible = true;
+            this.knifePrice.visible = true;
         }
     }
 }
